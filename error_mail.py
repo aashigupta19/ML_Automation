29bb7af8158eda0081781be63ced0af83cae4901
Aashi Gupta <64485386+aashigupta19@users.noreply.github.com>
2020-05-24 19:50:58 +0530

    Add files via upload

--- error_mail.py
+++ error_mail.py
@@ -33,7 +33,5 @@ session.login(sender_address, sender_pass) #login with mail_id and password
 text = message.as_string()
 
 session.sendmail(sender_address, receiver_address, text)
+print("error mail sent")
 session.quit()
-
-
-print('Mail Sent')
--- success_mail.py
+++ success_mail.py
@@ -33,7 +33,6 @@ session.login(sender_address, sender_pass) #login with mail_id and password
 text = message.as_string()
 
 session.sendmail(sender_address, receiver_address, text)
+print("success mail sent")
 session.quit()
-
-
-print('Mail Sent')
+ 
