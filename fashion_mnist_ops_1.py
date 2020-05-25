0ff7792e29413394e80ff8a74f8b85069fabb9e9
Aashi Gupta <64485386+aashigupta19@users.noreply.github.com>
2020-05-25 23:24:17 +0530

    Add files via upload

--- fashion_mnist_ops_1.py
+++ fashion_mnist_ops_1.py
@@ -1,217 +1,120 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ### Fashion MNIST
-
-# ### After downloading our dataset we see it's coded in the ubyte form
-# - We then use the following function to read the data and return it as a numpy array
-
 # In[1]:
 
 
-import struct
-import numpy as np
+from keras.models import Sequential
+from keras.layers import MaxPool2D, Conv2D, Flatten, Dense, Dropout
 from keras.datasets import fashion_mnist
+from keras.callbacks import Callback
+from keras.utils import np_utils
+from sklearn.metrics import accuracy_score
+import numpy as np
 
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 
 
-# ### We use the function to extact our training and test datasets
-
-# In[ ]:
-
-
-
-
-
-# ### Let's inspect our dataset
-
 # In[2]:
 
 
-# printing the number of samples in x_train, x_test, y_train, y_test
-print("Initial shape or dimensions of x_train", str(x_train.shape))
+x_train.shape
 
-print ("Number of samples in our training data: " + str(len(x_train)))
-print ("Number of labels in our training data: " + str(len(y_train)))
-print ("Number of samples in our test data: " + str(len(x_test)))
-print ("Number of labels in our test data: " + str(len(y_test)))
-print()
-print ("Dimensions of x_train:" + str(x_train[0].shape))
-print ("Labels in x_train:" + str(y_train.shape))
-print()
-print ("Dimensions of x_test:" + str(x_test[0].shape))
-print ("Labels in y_test:" + str(y_test.shape))
-
-
-# ### Let's create our model
 
 # In[3]:
 
 
-from keras.datasets import mnist
-from keras.utils import np_utils
-import keras
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-from keras import backend as K
+print("shape of training X", x_train.shape)
+print("shape of training Y", y_train.shape)
+print("shape of testing X", x_test.shape)
+print("shape of testing Y", y_test.shape)
 
-# Training Parameters
-#batch_size = 128
-#epochs = 1
-
-# Lets store the number of rows and columns
-img_rows = x_train[0].shape[0]
-img_cols = x_train[1].shape[0]
-
-# Getting our date in the right 'shape' needed for Keras
-# We need to add a 4th dimenion to our date thereby changing our
-# Our original image shape of (60000,28,28) to (60000,28,28,1)
-x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-
-# store the shape of a single image 
-input_shape = (img_rows, img_cols, 1)
-
-# change our image type to float32 data type
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-
-# Normalize our data by changing the range from (0 to 255) to (0 to 1)
-x_train /= 255
-x_test /= 255
-
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
-
-# Now we one hot encode outputs
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-
-# Let's count the number columns in our hot encoded matrix 
-print ("Number of Classes: " + str(y_test.shape[1]))
-
-num_classes = y_test.shape[1]
-num_pixels = x_train.shape[1] * x_train.shape[2]
-
-# create model
-model = Sequential()
-
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-
-
-
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(BatchNormalization())
-
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
-
-batch_size = 128
-epochs = 2
-model.compile(loss = 'categorical_crossentropy',
-              optimizer = keras.optimizers.Adadelta(),
-              metrics = ['accuracy'])
-
-print(model.summary())
-
-
-# ### Let's train our model
 
 # In[4]:
 
-#keep the varibales here so as to see the change
-history = model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+x_train_new = x_train.reshape(60000, 28, 28, 1)
+x_train_new = x_train_new.astype('float32')
+x_train_new = x_train_new/255.0
+
+x_test_new = x_test.reshape(10000, 28, 28, 1)
+x_test_new = x_test_new.astype('float32')
+x_test_new = x_test_new/255.0
 
 
 # In[5]:
 
 
-type(score)
+y_train.shape
 
 
 # In[6]:
 
 
-score[0]
+y_train_new = np_utils.to_categorical(y_train)
+y_test_new = np_utils.to_categorical(y_test)
 
 
 # In[7]:
 
 
-score
+print("shape of training X after processing", x_train_new.shape)
+print("shape of training Y after processing", y_train_new.shape)
+print("shape of testing X after processing", x_test_new.shape)
+print("shape of testing Y after processing", y_test_new.shape)
 
 
 # In[8]:
 
 
-score[1]
+#model creation
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), activation = 'relu', input_shape = (28,28,1) ))
+model.add(MaxPool2D(pool_size=(2, 2) ))
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dense(10,  activation = 'softmax'))          
 
 
 # In[9]:
 
 
-import numpy as np
+model.summary()
 
 
-# In[10]:
+# In[14]:
 
 
-ans=np.asarray(score)
-
-
-# In[11]:
-
-
-ans
-
-
-# In[12]:
-
-
-type(ans)
-ans[1]
-
-
-# In[13]:
-
-
-res = round(ans[1]*100 , 2)
-
-
-# In[ ]:
+class myCallback(Callback):
+      def on_epoch_end(self, epoch, logs={}):
+          
+            file='/root/accuracy.txt' 
+            var=logs.get('accuracy')
+            with open(file, 'w') as filetowrite:
+                filetowrite.write(np.array2string(var))
 
 
 
+
+callbacks = myCallback()
 
 
 # In[15]:
 
 
-np.savetxt('/root/accuracy.txt', [res])
+model.compile(optimizer='adam', loss = 'sparse_categorical_crossentropy', metrics=['accuracy'] )
+
+
+# In[16]:
+
+
+epochs = 10
+history = model.fit(x_train_new, y_train, epochs=epochs, callbacks=[callbacks])
 
 
 # In[ ]:
 
 
-
+#end of code :))))) 
 
